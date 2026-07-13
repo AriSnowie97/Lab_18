1de660cdde4e5d1c53a3c2a25cd252b956ce6e86
AriSnowie97 <diysylveon@gmail.com>
2026-07-13 22:23:08 +0300

    Fix uvicorn command in start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Запускаємо FastAPI у фоновому режимі (&)
-uvicorn api:app --host 0.0.0.0 --port ${PORT:-8000} &
+python -m uvicorn api:app --host 0.0.0.0 --port ${PORT:-8000} &
 
 # Запускаємо Telegram-бота
 python weather_bot.py

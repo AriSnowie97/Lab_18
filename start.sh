a3736ef0dabdf19fca1a9fd911cc4770fc179e67
AriSnowie97 <diysylveon@gmail.com>
2026-07-13 22:32:50 +0300

    Inline start script into Procfile

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Запускаємо Telegram-бота у фоні
-python weather_bot.py &
-
-# Запускаємо FastAPI на передньому плані (це важливо для Railway!)
-python -m uvicorn api:app --host 0.0.0.0 --port ${PORT:-8000}
-
